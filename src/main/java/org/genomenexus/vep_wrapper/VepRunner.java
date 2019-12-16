4beb6968007b4da18205ec2040f9d3f157a4870e
Robert Sheridan <sheridan@cbio.mskcc.org>
2019-12-16 05:04:55 -0500

    - add computeOrder() function : computes forward and backwards remapping of order - apply forward re-ordering (to establish chromosome position order) - backwards re-ordering (to re-establish the query order) not yet applied - add --fork 4 argument for parallel operations - add extra logging for testing

--- src/main/java/org/genomenexus/vep_wrapper/VepRunner.java
+++ src/main/java/org/genomenexus/vep_wrapper/VepRunner.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -16,24 +17,59 @@ import org.springframework.beans.factory.annotation.Value;
 public class VepRunner {
     public static final String VEP_DEFAULT_PARAMS = null;
 
+    private static int[] processingOrder = null; // a reordering of the request to put them into chromosomal order for processing
+    private static int[] responseOrder = null; // a reordering of the processing output to restore the original request order in our response
+
+    private static final String INDEX_DELIMITER = "#";
+
+    private static void computeOrders(List<String> requestList) {
+        ArrayList<String> workingRequestOrder = new ArrayList();
+        processingOrder = new int[requestList.size()];
+        responseOrder = new int[requestList.size()];
+        System.out.println("computing order of input list (list size: " + requestList.size() + ")");
+        int index = 0;
+        for (String request : requestList) {
+            workingRequestOrder.add(request + INDEX_DELIMITER + Integer.toString(index));
+            index = index + 1;
+        }
+        Collections.sort(workingRequestOrder);
+        int sortedIndex= 0;
+        for (String request : workingRequestOrder) {
+            String[] parts = request.split(INDEX_DELIMITER);
+            if (parts.length < 2) {
+                System.out.println("something bad happened during split of working order");
+                System.exit(3);
+            }
+            try {
+                int originalIndex = Integer.parseInt(parts[1]);
+                processingOrder[originalIndex] = sortedIndex;
+                responseOrder[sortedIndex] = originalIndex;
+            } catch (NumberFormatException e) {
+                System.out.println("something bad happened during parse of offset of working order");
+                System.exit(3);
+            }
+            sortedIndex = sortedIndex + 1;
+        }
+    }
+
     public static String run(List<String> regions, Boolean convertToListJSON) throws IOException, InterruptedException {
+        System.out.println("Running vep");
         // get vep pameters (use -Dvep.params to change)
         String vepParameters = System.getProperty("vep.params", String.join(" ",
             "--cache",
             "--offline",
             "--everything",
             "--hgvsg",
-            "--assembly",
-            "GRCh37",
-            "--format",
-            "region",
+            "--assembly GRCh37",
+            "--format region",
+            "--fork 4",
+            "--fasta /opt/vep/.vep/homo_sapiens/98_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz",
             "--json",
-            "-o",
-            "STDOUT",
+            "-o STDOUT",
             "--no_stats"
         ));
 
-        //Build command 
+        //Build command
         List<String> commands = new ArrayList<String>();
         commands.add("vep");
         for (String param : vepParameters.split(" ")) {
@@ -46,18 +82,26 @@ public class VepRunner {
             commands = replaceOptValue(commands, "--assembly", assembly);
         }
 
+        System.out.println("running command: " + commands);
         //Run macro on target
         ProcessBuilder pb = new ProcessBuilder(commands);
         pb.directory(new File("/opt/vep/src/ensembl-vep"));
         pb.redirectErrorStream(true);
+        System.out.println("staring..");
         Process process = pb.start();
 
+        // compute forward and backword reordering
+        computeOrders(regions);
+
         // send regions to stdin
+        System.out.println("processing requests");
         OutputStream stdin = process.getOutputStream();
         BufferedWriter stdinWriter = new BufferedWriter(new OutputStreamWriter(stdin));
-        for (String region: regions) {
+        for (int index : processingOrder) {
+            String region = regions.get(index);
             stdinWriter.write(region);
             stdinWriter.write("\n");
+            System.out.print(".");
         }
         stdinWriter.flush();
         stdinWriter.close();
@@ -74,6 +118,7 @@ public class VepRunner {
             if (previous != null && convertToListJSON) {
                 out.append(',');
                 out.append('\n');
+                System.out.print("o");
             }
             out.append(line);
             previous = line;
@@ -84,13 +129,18 @@ public class VepRunner {
         }
 
         // Check result
-        if (process.waitFor() == 0) {
-            //System.out.println("OK");
-            //System.out.println(out.toString());
+        int statusCode = process.waitFor();
+        if (statusCode == 0) {
+            System.out.println("OK");
+            System.out.println(out.toString());
             return out.toString();
         }
 
-        //Abnormal termination: Log command parameters and output and throw ExecutionException
+        //TODO: Abnormal termination: Log command parameters and output and throw ExecutionException
+        System.out.println("abnormal termination");
+        System.out.println("exited with status: " + statusCode);
+        System.out.println("returning output anyway:");
+        System.out.println(out.toString());
         return out.toString();
     }
 
